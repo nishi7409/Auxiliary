ce45d1fc64ef55b8c60008d3576936d733b51b81
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-04-06 19:46:43 +0530

    updates
    a lot of updates

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
+/*
+	Load in all packages and configuration files
+*/
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const Enmap = require("enmap");
+const fs = require("fs");
+const config = require("./settings/config.json");
 
-client.login(token);
+/*
+	Set a variable for the bot (in this instance, client)
+*/
+const client = new Discord.Client();
+// Implement config to the client so the config is valid everywhere
+client.config = config;
+
+/*
+	Events to be loaded in (message, memebrAdd, etc)
+*/
+fs.readdir("./events/", (err, files) => {
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+
+// All valid commands
+client.commands = new Enmap();
+
+/*
+	Commands to be loaded in (ping, verify, etc)
+*/
+fs.readdir("./commands/", (err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+
+// Client login
+client.login(config.bot_token);
