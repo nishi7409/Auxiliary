7ed1842cb01873548c39cf31e64ea60c9c7ca23d
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-05-05 21:01:25 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,9 +3,12 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
 
 	var inviteEmbed = new Discord.MessageEmbed()
-		.setColor(0x28F6FF)
-		.setDescription(`[Before you can invite this bot to your guild (and have it hosted for free), you have to sign up for a slot.  Ping a staff member and he/she will be delighted to help you out!](https://discord.gg/fHpfmy5)\n\n**Or pick up the source code @ https://github.com/nishi7409/Auxiliary**`)
-	return message.reply(inviteEmbed).then(message => message.delete({timeout: 5000, reason: "delete"}));
+    .setColor(0x28f6ff)
+    .setImage("https://media.giphy.com/media/xULW8MYvpNOfMXfDH2/giphy.gif")
+    .setDescription(
+      `[Before you can invite this bot to your guild (and have it hosted for free), you have to sign up for a slot.  Ping a staff member and he/she will be delighted to help you out!](https://discord.gg/fHpfmy5)\n\n**Or pick up the source code @ https://github.com/nishi7409/Auxiliary**`
+    );
+	return message.reply(inviteEmbed).then(message => message.delete({timeout:5000, reason: "delete"}));
 };
 
 exports.info = {
