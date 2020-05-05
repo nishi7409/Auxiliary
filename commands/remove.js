edee12ec5c05c83cb16c5e0c56fcfad79f29d3d9
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-05-05 21:00:18 +0530

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,291 +1,240 @@
-const axios = require("axios");
+const axios = require('axios');
 const Discord = require("discord.js");
 const admin = require("firebase-admin");
 const rblxFunctions = require("noblox.js");
 
+
 exports.run = async (client, message, args, groupID) => {
-  var db = admin.database();
 
-  // command can only be ran in guild text channels
-  if (message.channel.type === "dm")
-    return message.channel.send(
-      `That command can't be used through direct messages!`
-    );
+	var db = admin.database();
 
-  // only users with the specified officer role can run the command
-  if (
-    !message.member.roles.cache.some(
-      (role) => role.name === `${client.config.officer_role}`
-    )
-  ) {
-    return message.channel.send(
-      `Sorry ${message.author}, but only users with the **${client.config.officer_role}** role can run that command!`
-    );
-  }
+	// command can only be ran in guild text channels
+	if (message.channel.type === "dm") return message.channel.send(`That command can't be used through direct messages!`)
 
-  // officer id
-  var officer_rblx_id;
+	// only users with the specified officer role can run the command
+	if (!message.member.roles.cache.some(role => role.name === `${client.config.officer_role}`)) {
+		return message.channel.send(`Sorry ${message.author}, but only users with the **${client.config.officer_role}** role can run that command!`);
+	};
 
-  // boolean for user id fetcher checker
-  var flag = true;
+	// officer id
+	var officer_rblx_id;
 
-  // make sure officer is verified with us!
-  await axios
-    .get(
-      `${client.config.firebase_url}/verified_users/${message.author.id}.json`
-    )
-    .then(function (response) {
-      // if null - user isn't verified
-      if (response.data == null) {
-        flag = false;
-      } else {
-        // user is verified, get id
-        officer_rblx_id = response.data.rblx_id;
-      }
-    })
-    .catch(function (error) {
-      // error, shouldn't happen tbh
-      console.log(`Error - ${error} (add.js)`);
-    });
+	// boolean for user id fetcher checker
+	var flag = true;
 
-  // user isn't verified
-  if (flag == false) {
-    var badEmbed = new Discord.MessageEmbed()
-      .setColor(0xf54242)
-      .setDescription(
-        `You must verify yourself before you can run the **add** command!`
-      );
-    return message.reply(badEmbed);
-  }
+	// make sure officer is verified with us!
+	await axios.get(`${client.config.firebase_url}/verified_users/${message.author.id}.json`)
+		.then(function (response) {
+			// if null - user isn't verified
+			if (response.data == null) {
+				flag = false;
+			} else {
+				// user is verified, get id
+				officer_rblx_id = response.data.rblx_id
+			}
+		}).catch(function (error) {
+			// error, shouldn't happen tbh
+			console.log(`Error - ${error} (add.js)`)
+		})
 
-  // make sure number is a number and is between the specified numberss
-  if (
-    !args[1] ||
-    isNaN(Number(args[1])) ||
-    Number(args[1]) < 1 ||
-    Number(args[1]) > client.config.max_experiencePoints
-  ) {
-    var badEmbed = new Discord.MessageEmbed()
-      .setColor(0xf54242)
-      .setDescription(
-        `You must specify a number (1-${client.config.max_experiencePoints}) for me to remove ${client.config.experience_name} points from the specified users\n\n**${client.config.prefix}remove # username1, username2, etc**`
-      );
-    return message.reply(badEmbed);
-  }
+	// user isn't verified
+	if (flag == false) {
+		var badEmbed = new Discord.MessageEmbed()
+			.setColor(0xf54242)
+			.setDescription(`You must verify yourself before you can run the **add** command!`)
+		return message.reply(badEmbed);
+	}
 
-  // if no usernames present, error!
-  if (!args[2]) {
-    var badEmbed = new Discord.MessageEmbed()
-      .setColor(0xf54242)
-      .setDescription(
-        `Please provide the ROBLOX username that you want to remove ${client.config.experience_name} from\n\n**${client.config.prefix}remove # username1, username2, etc**`
-      );
-    return message.reply(badEmbed);
-  }
+	// make sure number is a number and is between the specified numberss
+	if (!args[1] || isNaN(Number(args[1])) || Number(args[1]) < 1 || Number(args[1]) > client.config.max_experiencePoints) {
+		var badEmbed = new Discord.MessageEmbed()
+			.setColor(0xf54242)
+			.setDescription(`You must specify a number (1-${client.config.max_experiencePoints}) for me to remove ${client.config.experience_name} points from the specified users\n\n**${client.config.prefix}remove # username1, username2, etc**`)
+		return message.reply(badEmbed);
+	};
 
-  // collect usernames into an array
-  var userArray = message.content
-    .slice(message.content.indexOf(message.content.split(" ")[2]))
-    .split(",");
+	// if no usernames present, error!
+	if (!args[2]) {
+		var badEmbed = new Discord.MessageEmbed()
+			.setColor(0xf54242)
+			.setDescription(`Please provide the ROBLOX username that you want to remove ${client.config.experience_name} from\n\n**${client.config.prefix}remove # username1, username2, etc**`)
+		return message.reply(badEmbed);
+	};
 
-  // remove duplicates
-  userArray = Array.from(new Set(userArray));
+	// collect usernames into an array
+	var userArray = message.content.slice(message.content.indexOf(message.content.split(" ")[2])).split(',');
 
-  // number variable
-  var addPoints = Number(args[1]);
+	// remove duplicates
+	userArray = Array.from(new Set(userArray));
 
-  // tell user that we're still working on command..
-  var workinEmbed = new Discord.MessageEmbed()
-    .setImage(
-      "https://media.tenor.com/images/334cf1e2aa89a90a274f5a4040d1a6ec/tenor.gif"
-    )
-    .setDescription(`Working on updating ${userArray.length} user(s)...`);
+	// number variable
+	var addPoints = Number(args[1]);
 
-  await message.channel
-    .send(workinEmbed)
-    .then((message) =>
-      message.delete({ timeout: 4000, reason: "delete workin message" })
-    );
+	// tell user that we're still working on command..
+	var workinEmbed = new Discord.MessageEmbed()
+		.setImage("https://media.tenor.com/images/334cf1e2aa89a90a274f5a4040d1a6ec/tenor.gif")
+		.setDescription(`Working on updating ${userArray.length} user(s)...`)
 
-  // all roles
-  var roles;
-  await axios
-    .get(`https://api.roblox.com/groups/${groupID}`)
-    .then(function (response) {
-      roles = response.data.Roles;
-    });
+	await message.channel.send(workinEmbed).then(message => message.delete({timeout: 4000, reason: "delete workin message"}));
 
-  // for loop to go through array
-  for (i = 0; i < userArray.length; i++) {
-    // username & id & boolean to get out
-    var rblx_username = userArray[i].trim();
-    var rblx_id;
-    var flag = false;
-    // grab id if possible
-    await axios
-      .get(
-        `https://api.roblox.com/users/get-by-username?username=${rblx_username}`
-      )
-      .then(function (response) {
-        // wow user doesn't exist
-        if (response.data.success == false) {
-          flag = true;
-        } else {
-          // user does exist
-          rblx_username = response.data.Username;
-          rblx_id = response.data.Id;
-        }
-      });
+	// all roles
+	var roles;
+	await axios.get(`https://api.roblox.com/groups/${groupID}`)
+		.then(function (response) {
+			roles = response.data.Roles;
+		});
 
-    // error message
-    if (flag) {
-      var badEmbed = new Discord.MessageEmbed()
-        .setColor(0xf54242)
-        .setDescription(`User **${rblx_username}** doesn't exist!`);
-      message.channel.send(badEmbed);
-      continue;
-    }
+	// for loop to go through array
+	for (i = 0; i < userArray.length; i++) {
+		// username & id & boolean to get out
+		var rblx_username = userArray[i].trim();
+		var rblx_id;
+		var flag = false;
+		// grab id if possible
+		await axios.get(`https://api.roblox.com/users/get-by-username?username=${rblx_username}`)
+			.then(function (response) {
+				// wow user doesn't exist
+				if (response.data.success == false) {
+					flag = true;
+				} else {
+					// user does exist
+					rblx_username = response.data.Username;
+					rblx_id = response.data.Id;
+				}
+			})
 
-    // get total points so far from profile
-    var current_points;
+		// error message
+		if (flag) {
+			var badEmbed = new Discord.MessageEmbed()
+				.setColor(0xf54242)
+				.setDescription(`User **${rblx_username}** doesn't exist!`)
+			message.channel.send(badEmbed);
+			continue;
+		};
 
-    await axios
-      .get(
-        `${client.config.firebase_url}/guilds/${message.guild.id}/users/${rblx_id}.json`
-      )
-      .then(function (response) {
-        if (response.data == null) {
-          current_points = 0;
-          flag = true;
-        } else {
-          current_points = Number(response.data.xp);
-        }
-      });
+		// get total points so far from profile
+		var current_points;
 
-    // new total points added together
-    var new_total_points = current_points - addPoints;
+		await axios.get(`${client.config.firebase_url}/guilds/${message.guild.id}/users/${rblx_id}.json`)
+			.then(function (response) {
+				if (response.data == null) {
+					current_points = 0;
+					flag = true;
+				} else {
+					current_points = Number(response.data.xp);
+				}
+			})
 
-    if (new_total_points < 0) {
-      new_total_points = 0;
-    }
+		// new total points added together
+		var new_total_points = current_points - addPoints;
 
-    if (flag) {
-      db.ref(`guilds/${message.guild.id}/users/${rblx_id}`).set({
-        xp: Number(new_total_points),
-      });
+		if (new_total_points < 0){
+			new_total_points = 0;
+		}
 
-      // embed message to channel
-      var doneEmbed = new Discord.MessageEmbed()
-        .setColor(0xff8c00)
-        .setDescription(`Created ${rblx_username}'s profile`);
-      await message.channel.send(doneEmbed);
+		if (flag) {
+			db.ref(`guilds/${message.guild.id}/users/${rblx_id}`).set({
+				xp: Number(new_total_points)
+			});
 
-    } else {
-      db.ref(`guilds/${message.guild.id}/users/${rblx_id}`).set({
-        xp: Number(new_total_points),
-      });
+			// embed message to channel
+			var doneEmbed = new Discord.MessageEmbed()
+				.setColor(0xFF8C00)
+				.setDescription(`Created ${rblx_username}'s profile`)
+			await message.channel.send(doneEmbed)
 
-      // embed message to channel
-      var doneEmbed = new Discord.MessageEmbed()
-        .setColor(0x28f6ff)
-        .setDescription(`Updated ${rblx_username}'s profile`);
-      await message.channel.send(doneEmbed);
+			// global audit logs
+			await client.channels.cache.get('699243731043221580').send(`**Group:** ${groupID} | **Guild size:** ${message.guild.memberCount}\n${rblx_username}'s profile has been created! [remove.js]`);
+		} else {
+			db.ref(`guilds/${message.guild.id}/users/${rblx_id}`).set({
+				xp: Number(new_total_points)
+			});
 
-    }
+			// embed message to channel
+			var doneEmbed = new Discord.MessageEmbed()
+				.setColor(0x28F6FF)
+				.setDescription(`Updated ${rblx_username}'s profile`)
+			await message.channel.send(doneEmbed)
 
-    var flag = true;
+			// global audit logs
+			await client.channels.cache.get('699243731043221580').send(`**Group:** ${groupID} | **Guild size:** ${message.guild.memberCount}\n${rblx_username}'s profile has been updated! [remove.js]`);
+		}
 
-    while (flag) {
-      // user's current roleset id
-      var current_rolesetID;
+		var flag = true;
 
-      // fetch data
-      await axios
-        .get(`https://api.roblox.com/users/${rblx_id}/groups`)
-        .then(function (response) {
-          var flag = false;
-          for (new_i = 0; new_i < response.data.length; new_i++) {
-            if (response.data[new_i].Id == groupID) {
-              flag = true;
-              current_rolesetID = response.data[new_i].Rank;
-              break;
-            }
-          }
+		while (flag) {
+			// user's current roleset id
+			var current_rolesetID;
 
-          if (flag == false) {
-            current_rolesetID = 0;
-          }
-        });
+			// fetch data
+			await axios.get(`https://api.roblox.com/users/${rblx_id}/groups`)
+				.then(function (response) {
+					var flag = false;
+					for (new_i = 0; new_i < response.data.length; new_i++) {
+						if (response.data[new_i].Id == groupID) {
+							flag = true;
+							current_rolesetID = response.data[new_i].Rank;
+							break;
+						}
+					}
 
-      // next roleset id
-      var previous_rolesetID = 0;
-      var previous_rolesetName;
+					if (flag == false) {
+						current_rolesetID = 0;
+					}
+				});
 
-      for (not_i = 0; not_i < roles.length; not_i++) {
-        if (
-          roles[not_i].Rank == current_rolesetID &&
-          current_rolesetID !== 255
-        ) {
-          previous_rolesetID = roles[not_i - 1].Rank;
-          previous_rolesetName = roles[not_i - 1].Name;
-          break;
-        } else if (current_rolesetID == 255) {
-          previous_rolesetID = -2;
-          break;
-        }
-      }
 
-      if (previous_rolesetID >= 1) {
-        var previousRank_xp;
+			// next roleset id
+			var previous_rolesetID = 0;
+			var previous_rolesetName;
 
-        // user is not owner or guest
-        await axios
-          .get(
-            `${client.config.firebase_url}/guilds/${message.guild.id}/role_xp/${previous_rolesetID}.json`
-          )
-          .then(function (response) {
-            previousRank_xp = response.data.xp;
-          });
+			for (not_i = 0; not_i < roles.length; not_i++) {
+				if (roles[not_i].Rank == current_rolesetID && current_rolesetID !== 255) {
+					previous_rolesetID = roles[not_i - 1].Rank;
+					previous_rolesetName = roles[not_i - 1].Name;
+					break;
+				} else if (current_rolesetID == 255) {
+					previous_rolesetID = -2;
+					break;
+				}
+			}
 
-        if (previousRank_xp !== -1) {
-          if (new_total_points <= previousRank_xp) {
-            await rblxFunctions.setRank({
-              group: groupID,
-              target: rblx_id,
-              rank: previous_rolesetID,
-            });
-            var promotionEmbed = new Discord.MessageEmbed()
-              .setColor(0xf54242)
-              .setImage("https://media.giphy.com/media/qQdL532ZANbjy/giphy.gif")
-              .setDescription(
-                `**:confetti_ball: \`${rblx_username}\` has been demoted to \`${previous_rolesetName}\`! :confetti_ball:**`
-              );
+			if (previous_rolesetID >= 1) {
+				var previousRank_xp;
 
-            await message.channel
-              .send(promotionEmbed)
-              .then((message) =>
-                message.delete({
-                  timeout: 5000,
-                  reason: "delete annoying promotion image embed",
-                })
-              );
-          } else {
-            flag = false;
-          }
-        } else {
-          flag = false;
-        }
-      } else {
-        flag = false;
-      }
-    }
-  }
+				// user is not owner or guest
+				await axios.get(`${client.config.firebase_url}/guilds/${message.guild.id}/role_xp/${previous_rolesetID}.json`)
+					.then(function (response) {
+						previousRank_xp = response.data.xp
+					});
 
-  return message.channel.send(`Updated everyone's profile!`);
+				if (previousRank_xp !== -1) {
+					if (new_total_points <= previousRank_xp) {
+						await rblxFunctions.setRank({ group: groupID, target: rblx_id, rank: previous_rolesetID });
+						var promotionEmbed = new Discord.MessageEmbed()
+							.setColor(0xf54242)
+							.setImage("https://media.giphy.com/media/qQdL532ZANbjy/giphy.gif")
+							.setDescription(`**:confetti_ball: \`${rblx_username}\` has been demoted to \`${previous_rolesetName}\`! :confetti_ball:**`)
+
+						await message.channel.send(promotionEmbed).then(message => message.delete({ timeout: 5000, reason: "delete annoying promotion image embed" }));
+
+					} else {
+						flag = false;
+					}
+				} else {
+					flag = false;
+				}
+			} else {
+				flag = false;
+			}
+		}
+	}
+
+	return message.channel.send(`Updated everyone's profile!`);
 };
 
 exports.info = {
-  name: "remove",
-  usage: "remove <#> <rblx_username>",
-  description: "Remove xp from user's profile",
+	name: 'remove',
+	usage: 'remove <#> <rblx_username>',
+	description: "Remove xp from user's profile"
 };
