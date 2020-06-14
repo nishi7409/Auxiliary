3287abee11fc321768452dd90d2bba8c31d41bcc
Conmmander <rcd0101@yahoo.com>
2020-06-14 13:17:21 -0500

    index.js
    Updated to make use of refreshCookie()
    
    Requires a new package (node-cron; allows timed events)

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const Discord = require("discord.js");
 const Enmap = require("enmap");
 const admin = require("firebase-admin");
 const rblxFunctions = require("noblox.js");
+const cron = require('node-cron');
 const fs = require("fs");
 
 var serviceAccount = require("./settings/serviceAccountKey.json");
 const config = require("./settings/config.json");
+
+// Setup so that you know if the bot is logged in. Mainly used to keep cookie validation running properly.
+var loggedIn = false
 
 // Set a variable for the bot (in this instance, client)
 const client = new Discord.Client();
 
 // Implement config to the client so the config is valid everywhere
 client.config = config;
-
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -22,10 +25,14 @@ admin.initializeApp({
 
 async function rblx_login(){
   await rblxFunctions.setCookie(config.rblx_cookie)
-  //await rblxFunctions.cookieLogin(config.rblx_cookie);
+  loggedIn = true
   console.log("logged in");
 }
 rblx_login();
+
+
+//var newCookie = rblxFunctions.refreshCookie(config.rblx_cookie)
+//console.log(newCookie)
 
 // Events to be loaded in (message, memebrAdd, etc)
 fs.readdir("./events/", (err, files) => {
@@ -50,6 +57,15 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-
 // Client login
 client.login(config.bot_token);
+
+// Initiate Cookie Refresh checking
+cron.schedule('* */1 * * *', () => {
+  if (loggedIn == true) {
+    rblxFunctions.refreshCookie().then(function(newCookie) {
+      config.rblx_cookie = newCookie
+      console.log("Cookie refreshed and validated.")
+    })
+  }
+});
