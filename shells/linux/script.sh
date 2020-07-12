a930fb58ab080ddd766cc5ca9ac679ef1ef43601
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-07-12 14:05:15 +0530

    Update script.sh

--- shells/linux/script.sh
+++ shells/linux/script.sh
@@ -7,6 +7,10 @@ echo -e "Installing files..."
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install
+sudo apt-get install gcc g++ make
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs 
 sudo apt install -y nodejs npm git
